a38e9e062358803d273723584ac9172bd1c01ec5
Yaszu <76853160+Yaszu-Mage@users.noreply.github.com>
2025-02-12 22:19:40 -0800

    Fixed Items

--- src/main/java/space/yaszu/yahoo/items/item_event.java
+++ src/main/java/space/yaszu/yahoo/items/item_event.java
@@ -1,37 +1,43 @@
 package space.yaszu.yahoo.items;
 
 import de.tr7zw.nbtapi.NBT;
-import org.bukkit.Location;
-import org.bukkit.Particle;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import space.yaszu.yahoo.Yahoo;
 
 public class item_event implements Listener {
+    @EventHandler
     public void right_click(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        ItemStack item = event.getItem();
+        ItemStack item = player.getItemInHand();
         String id;
-        Boolean nbt_val = NBT.get(item, nbt -> {
-            return nbt.hasTag("Yah_ID");
-        });
-        if (nbt_val) {
-            id = NBT.get(item,nbt ->{
-              return nbt.getString("Yah_ID");
-            });
-            switch (id) {
-                case "coke":
-                    item.subtract(1);
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,500,1));
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.NAUSEA,500,1));
-                case "spawn_cry":
-                    item.subtract(1);
-                    player.spawnParticle(Particle.PORTAL,player.getLocation(),64);
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING,1000,1));
-                    player.teleport(new Location(player.getWorld(),0, 100,0));
+        if (item.getAmount() != 0) {
+
+
+            ItemMeta meta = item.getItemMeta();
+            NamespacedKey key2 = new NamespacedKey(Bukkit.getPluginManager().getPlugin("Yahoo"),"Yah_ID");
+            id = meta.getPersistentDataContainer().get(key2, PersistentDataType.STRING);
+            space.yaszu.yahoo.Yahoo.getPlugin(Yahoo.class).getLogger().info(id + " was used");
+            if (id.equals("coke")) {
+                item.subtract(1);
+                player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,500,1));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.NAUSEA,500,1));
+            }
+            if (id.equals("spawn_cry")) {
+                item.subtract(1);
+                player.getWorld().playSound(player.getLocation(), Sound.BLOCK_PORTAL_TRAVEL, 1f, 1f);
+                player.spawnParticle(Particle.PORTAL,player.getLocation(),64);
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING,1000,1));
+                player.teleport(new Location(player.getWorld(),0, 100,0));
+                player.getWorld().playSound(player.getLocation(), Sound.BLOCK_PORTAL_TRAVEL, 1f, 1f);
             }
 
         }
