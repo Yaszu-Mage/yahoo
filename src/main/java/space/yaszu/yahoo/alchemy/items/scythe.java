7426318a4f40de0d4d2561909eab3ccdb1555d47
Yaszu <76853160+Yaszu-Mage@users.noreply.github.com>
2025-04-12 12:42:28 -0700

    implemented the cooldown

--- src/main/java/space/yaszu/yahoo/alchemy/items/scythe.java
+++ src/main/java/space/yaszu/yahoo/alchemy/items/scythe.java
@@ -1,10 +1,8 @@
 package space.yaszu.yahoo.alchemy.items;
 
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Entity;
@@ -15,16 +13,27 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+import space.yaszu.yahoo.Yahoo;
 import space.yaszu.yahoo.key;
 import org.bukkit.util.Vector;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 public class scythe implements Listener {
     public static key key = new key();
+    public HashMap<UUID, Long> cooldowns = new HashMap<>();
+    private final long cooldownTime = 10000;
+    public NamespacedKey nofall = key.get_key("nofall");
+
+
+
     public ItemStack scythe_item() {
         ItemStack item = ItemStack.of(Material.DIAMOND_SWORD);
         ItemMeta meta = item.getItemMeta();
+        meta.getPersistentDataContainer().set(key.get_key("scythe"),PersistentDataType.BOOLEAN,true);
         meta.addAttributeModifier(Attribute.ATTACK_DAMAGE,new AttributeModifier(key.get_key("scythe_damage"),1, AttributeModifier.Operation.ADD_NUMBER));
         meta.addAttributeModifier(Attribute.ATTACK_SPEED, new AttributeModifier(key.get_key("scythe_speed"),-1, AttributeModifier.Operation.ADD_NUMBER));
         meta.displayName(MiniMessage.miniMessage().deserialize("<gray>|</gray><gradient:#ff55ff:#6c00aa> Scythe<reset> <gray>|</gray>"));
@@ -35,8 +44,22 @@ public class scythe implements Listener {
 
     @EventHandler
     public void event(PlayerInteractEvent event){
+        long currentTime = System.currentTimeMillis();
         Player player = event.getPlayer();
+        UUID playerUUID = player.getUniqueId();
+        if (cooldowns.containsKey(playerUUID)) {
+
+            long lastUsed = cooldowns.get(playerUUID);
+            long timeLeft = cooldownTime - (currentTime - lastUsed);
+            if (timeLeft > 0) {
+                double secondsLeft = timeLeft / 1000.0;
+                player.sendActionBar(Component.text("§cCooldown: " + String.format("%.1f", secondsLeft) + "s"));
+                return;
+            }
+        }
+        if (player.getInventory().getItemInMainHand().getPersistentDataContainer().has(key.get_key("scythe"))) {
         if (player.isSneaking()) {
+            cooldowns.put(playerUUID,currentTime);
             Location startlocation = player.getLocation();
             Vector direction = startlocation.getDirection().normalize().multiply(2);
             Location targetLocation = startlocation.add(direction);
@@ -46,6 +69,7 @@ public class scythe implements Listener {
                 player.getWorld().spawnParticle(Particle.SOUL,step,48,0.2,0.2,0.2,0.1);
             }
             player.setVelocity(direction);
+            player.getPersistentDataContainer().set(nofall, PersistentDataType.BOOLEAN,true);
             player.getWorld().playSound(startlocation, Sound.ENTITY_WITHER_HURT,1,1);
             List<Entity> entities = player.getNearbyEntities(5,2,5);
             for (Entity entity : entities) {
@@ -58,7 +82,12 @@ public class scythe implements Listener {
                     break;
                 }
             }
-
+            Bukkit.getScheduler().runTaskLater(Yahoo.get_plugin(), new Runnable() {
+                @Override
+                public void run() {
+                   player.getPersistentDataContainer().remove(nofall);
+                }
+            },100);
         }
-    }
+    }}
 }
